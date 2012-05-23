f7b9448b437ba941fdd645cce7a72e9f3c34baf4
Saurabh <saurabh@letsalign.com>
2012-05-23 17:51:38 +0530

    demo

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def home
+  	#@tomorrow = %w{aaa bbb ccc}
+	#@this_month = %w{iii jjj kkk}
+	#@next_month = %w{xxx yyy zzz}
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gbtp::Application.config.secret_token = '49c2b047fb33356c10b5a3a030020af9c002419c98609b8967f033ebf9ba16054b3cb2fbc28542930a3dcf8157268a5b5284938f4f0986a3e8091a2969505ade'
